679cce5b3a90a4621f2a0d586d34df38fb9ad323
Hans Yadav <hansy@users.noreply.github.com>
2022-08-10 18:03:52 -0400

    copy

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="text-center mt-8 mb-20 lg:mt-24 lg:mb-40">
       <Container>
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-50 sm:text-5xl md:text-6xl">
-          <span className="block mb-4">Manage your videos in one place.</span>
+          <span className="block mb-4">Play your videos anywhere.</span>
           <span className="block xl:inline">
             Get paid <span className="rainbow">everywhere</span>.
           </span>
@@ -41,8 +41,8 @@ const Header = () => {
           .{" "}
         </p>
         <p className="mt-16 max-w-xl mx-auto text-xl text-gray-200 md:mt-20 md:text-2xl">
-          Loop gives creators the tools to manage, monetize, and distribute
-          their content.
+          Loop lets you (and your fans) embed your videos on any website and get
+          paid together.
         </p>
         <div className="mt-5 mx-auto sm:flex sm:justify-center md:mt-5">
           <CtaForm buttonLabel="Join beta" placeholder="Email" />
